a119c5c1e42fb045e878bbb5192e7a6a44e0e4b9
kyeonghwan <nnoohh4095@gmail.com>
2025-11-07 19:43:24 +0900

    import, data load2

--- svm.py
+++ svm.py
@@ -7,3 +7,13 @@ from sklearn.metrics import confusion_matrix, f1_score, accuracy_score, roc_curv
 import warnings
 import matplotlib.pyplot as plt
 warnings.filterwarnings('ignore')
+
+data_url="https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.csv"
+col_names=['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age', 'Outcome']
+try:
+    data=pd.read_csv(data_url, header=None, names=col_names)
+    print("데이터 로드 완료. (총 {}개 샘플)",format(len(data)))
+except Exception as e:
+    print(f"데이터 로드 중 오류 발생: {e}")
+    print("로컬 환경에서 직접 CSV 파일을 다운로드하여 경로를 지정하거나, 데이터셋을 확인해 주세요.")
+    exit()
